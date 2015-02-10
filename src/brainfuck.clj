6972f2f546a86c09f1390a4c1ad92f4db7c3d259
Niclas Nilsson <niclas.love.nilsson@gmail.com>
2015-02-10 17:22:38 +0100

    Use idiomatic docstrings

--- src/brainfuck.clj
+++ src/brainfuck.clj
@@ -1,8 +1,8 @@
 (ns brainfuck
   (:require [clojure.core.match :refer [match]]))
 
-;; Return index of the matching bracket at index ip in source code c.
 (defn matching-bracket-idx [ip c]
+  "Returns the index of the matching bracket found at index ip in source code string c."
   (let [[t t' d] (if (= (get c ip) \[) [\[ \] 1] [\] \[ -1])]
     (loop [ip (+ ip d) n 0]
       (match [(get c ip) n]
@@ -11,32 +11,33 @@
              [t  _] (recur (+ ip d) (inc n))
              :else  (recur (+ ip d) n)))))
 
-;; Map operators to functions that update a state.
-(def ops {\> #(update-in % [:dp] inc)
-          \< #(update-in % [:dp] dec)
-          \+ #(update-in % [:t (:dp %)] (fnil inc 0))
-          \- #(update-in % [:t (:dp %)] (fnil dec 0))
-          \[ #(when (zero? ((:t %) (:dp %) 0))
-                (update-in % [:ip] matching-bracket-idx (:c %)))
-          \] #(when-not (zero? ((:t %) (:dp %) 0))
-                (update-in % [:ip] matching-bracket-idx (:c %)))
-          \. #(print (char ((:t %) (:dp %) 0)))
-          \, #(update-in % [:t (:dp %)] (byte (.read System/in)))})
+(def ops
+  "Map operators to functions that update state."
+  {\> #(update-in % [:dp] inc)
+   \< #(update-in % [:dp] dec)
+   \+ #(update-in % [:t (:dp %)] (fnil inc 0))
+   \- #(update-in % [:t (:dp %)] (fnil dec 0))
+   \[ #(when (zero? ((:t %) (:dp %) 0))
+         (update-in % [:ip] matching-bracket-idx (:c %)))
+   \] #(when-not (zero? ((:t %) (:dp %) 0))
+         (update-in % [:ip] matching-bracket-idx (:c %)))
+   \. #(print (char ((:t %) (:dp %) 0)))
+   \, #(update-in % [:t (:dp %)] (byte (.read System/in)))})
 
-;; Given a program state generate the next state. May produce side effects.
 (defn step [{c :c ip :ip :as state}]
+  "Return the next state from a given state. May produce side effects."
   (update-in (if-let [new-state ((ops (get c ip) identity) state)]
                new-state state)
              [:ip] inc))
 
-;; Lazily generate a sequence of states from source code.
-;; Fetching a state may produce side effects.
 (defn state-seq [source-code]
+  "Lazily generate a sequence of states from a string of source code.
+  Evaluating a state may produce side effects."
   ((fn generate [{c :c ip :ip :as state}]
      (when (< ip (count c))
        (cons state (lazy-seq (generate (step state))))))
    {:c (apply str (filter ops source-code)) :t {} :ip 0 :dp 0}))
 
-;; Interpret and run source code.
 (defn run [source-code]
+  "Interpret and run source code."
   (dorun (state-seq source-code)))
