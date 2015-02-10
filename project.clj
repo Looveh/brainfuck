52f76e37e605949f41be41840af926e963aabc71
Niclas Nilsson <niclas.love.nilsson@gmail.com>
2015-02-10 13:48:43 +0100

    Cleanup project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,3 @@
-(defproject brainfuck "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject brainfuck "0.0.1"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]])
